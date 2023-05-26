ec45a4fe716960e66c6e5a07f39e68f711a2e0cc
dmitrymashkalo <dmitrymashkalo@gmail.com>
2023-05-27 00:54:04 +0200

    Add practice: min and max.

---
+++ chapter_1_built_in/min_and_max.py
@@ -0,0 +1,20 @@
+# Minimum and Maximum
+
+playerOneScore = 10
+playerTwoScore = 7
+
+# Minimum
+print(min(playerOneScore, playerTwoScore))
+print(min(3, 32, 14, 8, 0, 56, -3, 99))
+
+# A-Z -> 0-26
+print(min('Kathryn', 'Katie'))
+print(min('Angela', 'Bob'))
+
+# Maximum
+print(max(playerOneScore, playerTwoScore))
+print(max(3, 32, 14, 8, 0, 56, -3, 99))
+
+# A-Z -> 0-26
+print(max('Kathryn', 'Katie'))
+print(max('Angela', 'Bob'))
