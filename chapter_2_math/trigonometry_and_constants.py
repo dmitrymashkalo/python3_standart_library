93dd47915b0c75f4b6ef69a0e4ae0ba19d2d3392
dmitrymashkalo <dmitrymashkalo@gmail.com>
2023-05-27 17:34:51 +0200

    Add practice: Chapter 2, trigonometry and constants.

---
+++ chapter_2_math/trigonometry_and_constants.py
@@ -0,0 +1,14 @@
+# Math Module
+import math
+
+# Constants
+print(math.pi) # Constant pi, the ratio of the circumference of a circle to its diameter.
+print(math.e) # Constant e, the base of the natural logarithm.
+print(math.nan) # Not a number (NaN), the result of invalid or undefined mathematical operations.
+print(math.inf) # Infinity, the result of infinite mathematical operations.
+print(-math.inf) # Negative infinity, the negative result of infinite mathematical operations.
+
+# Trigonometry
+obst_direction = math.cos(math.pi / 4) # The cosine of π/4 (45 degrees).
+print(obst_direction)
+print(math.sin(math.pi / 4)) # Calculates and prints the sine of π/4 (45 degrees).
