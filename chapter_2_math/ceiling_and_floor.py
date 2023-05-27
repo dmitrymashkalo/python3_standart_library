8c283c19fca7b6d714dfe89f1c3c8631e52cdfcd
dmitrymashkalo <dmitrymashkalo@gmail.com>
2023-05-27 17:35:26 +0200

    Add practice: Chapter 2, ceiling and floor.

---
+++ chapter_2_math/ceiling_and_floor.py
@@ -0,0 +1,16 @@
+# Math Module
+import math
+
+# Ceiling
+cookies = 10.3
+candy = 7
+
+print(math.ceil(cookies)) # Rounds up the value
+print(math.ceil(candy)) # Rounds up the value
+
+# Floor
+ageOne = 17.9
+ageTwo = 19
+
+print(math.floor(ageOne)) # Rounds down the value
+print(math.floor(ageTwo)) # Rounds down the value
