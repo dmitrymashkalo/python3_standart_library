2266d66979806c26629dd0014af4b453e8db5d2c
dmitrymashkalo <dmitrymashkalo@gmail.com>
2023-05-27 19:12:45 +0200

    Add practice: Chapter 2, factorial, sqrt, gcd, radians and degrees.

---
+++ chapter_2_math/factorial_square_root_and_gcd.py
@@ -0,0 +1,16 @@
+# Math Module
+import math
+
+# Factorial & Square Root
+print(math.factorial(3)) # Calculates the factorial of 3 (3!)
+print(math.sqrt(64)) # Calculates the square root of 64
+
+# Greatest Common Denominator
+print(math.gcd(52, 8)) # Finds the greatest common divisor of 52 and 8
+print(math.gcd(8, 52)) # Order doesn't matter
+
+# Degrees and Radians
+print(math.radians(360)) # Converts 360 degrees to radians
+print(math.pi * 2) # Calculates the value of 2π, which represents a full circle in radians
+
+print(math.degrees(math.pi * 2)) # Converts 2π radians to degrees
